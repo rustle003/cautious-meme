6cba377fd6fc2c9b44abfbc5772942c798b6cc77
Ramirez, Russell <rrussell.public@gmail.com>
2026-06-04 03:10:38 -0500

    Minor changes to ease the use of the repo factory.

--- src/model_pull/repo_interface.py
+++ src/model_pull/repo_interface.py
@@ -3,7 +3,7 @@ import tomllib
 import huggingface_hub
 from dataclasses import dataclass
 from pathlib import Path
-from typing import Protocol, Any, Type, override
+from typing import Protocol, Any, Type, override, Callable, Union
 
 
 @dataclass(frozen=True)
@@ -37,6 +37,24 @@ class RepoInterface(Protocol):
         raise FileNotFoundError(f"'${path}' does not exist, there are insufficient" +
                                 "permissions to access the file or a parent directory, " +
                                 "or the file name contains invalid characters.")
+
+class Repo[T : RepoInterface]:
+    def __init__(self: "Repo", repo: T, repo_config: RepoConfig) -> None:
+        self.repo: T = repo
+        self.pull_repo_data: Union[Callable[[],None], Callable[[],None]]= \
+            (lambda : repo.clone_repo(
+                repo_config.repo_id,
+                repo_config.download_path,
+                repo_config.revision
+            )) \
+            if repo_config.repo_file == "" \
+            else (lambda : repo.pull_file(
+                repo_config.repo_id,
+                repo_config.repo_file,
+                repo_config.download_path,
+                repo_config.revision
+            ))
+
 
 
 class HuggingFaceRepoInterface(RepoInterface):
@@ -72,7 +90,7 @@ class RepoFactory:
     def __init__(self: "RepoFactory", repo_config: RepoConfig) -> None:
         self.repo_config: RepoConfig = repo_config
 
-    def build_repo_interface(self: "RepoFactory") -> RepoInterface:
+    def build_repo_interface[T : RepoInterface](self: "RepoFactory") -> Repo[T]:
         """Create concrete repo interface if repo provider is known
 
         :param repo_provider: str
@@ -83,7 +101,7 @@ class RepoFactory:
             case "huggingface":
                 repo_interface: RepoInterface = HuggingFaceRepoInterface()
                 repo_interface.validate_configuration(self.repo_config)
-                return repo_interface
+                return Repo(repo_interface, self.repo_config)
             case _:
                 raise RepoFactory.UnknownRepoProviderException(
                     f"Unknown repo provider: {self.repo_config.repo_provider}. Known repo providers are: 'huggingface'."
@@ -100,8 +118,8 @@ def main(args: list[str]) -> int:
 
     repo_config: RepoConfig = RepoConfig.from_toml(toml_path)
     repo_factory: RepoFactory = RepoFactory(repo_config)
-    repo_interface: RepoInterface = repo_factory.build_repo_interface()
-    repo_interface.clone_repo(repo_config.repo_id, repo_config.download_path)
+    repo_interface: Repo[RepoInterface] = repo_factory.build_repo_interface()
+    repo_interface.pull_repo_data()
 
     return 0
 
