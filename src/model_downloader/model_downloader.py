1932f5855191f97c7db34c2c248377d0a576e102
Ramirez, Russell <rrussell.public@gmail.com>
2026-06-12 02:01:20 -0500

    Added a python script to bootstrap the model download process.

---
+++ src/model_downloader/model_downloader.py
@@ -0,0 +1,101 @@
+import venv
+import sys
+import subprocess
+import importlib.resources
+import tomllib
+from uuid import uuid4
+from pathlib import Path
+from typing import Any
+
+
+class VenvManager:
+    def __init__(self, prefix: str) -> None:
+        self.prefix: str = prefix
+
+        pre_existing_venv: str | None = self._find_existing_venv()
+
+        self.venv_dir: str = pre_existing_venv if pre_existing_venv is not None \
+            else self._generate_venv_dir_name()
+
+    def get_venv_dir(self: "VenvManager") -> str:
+        return self.venv_dir
+
+    def get_prefix(self: "VenvManager") -> str:
+        return self.prefix
+
+    def create_venv(self: "VenvManager") -> None:
+        # create virtual environment
+        venv_builder: venv.EnvBuilder = venv.EnvBuilder(with_pip=True)
+        venv_builder.create(self.venv_dir)
+
+    def remove_venv(self: "VenvManager") -> None:
+        VenvManager._rmdir(Path(self.venv_dir))
+
+    def _find_existing_venv(self: "VenvManager") -> str | None:
+        for possible_env in Path.cwd().glob(f"{self.prefix}_??????????????"):
+            if possible_env.is_dir():
+                return possible_env.as_posix()
+        return None
+
+    def _generate_venv_dir_name(self: "VenvManager") -> str:
+        return f".{self.prefix}_{
+        uuid4().urn
+        .split(":")
+        .pop()
+        .replace("-", "")[:14]
+        }"
+
+    @classmethod
+    def _rmdir(cls: "VenvManager", dir_path: Path) -> None:
+        for item in dir_path.glob("*"):
+            if item.is_dir():
+                VenvManager._rmdir(item)
+            else:
+                item.unlink()
+        dir_path.rmdir()
+
+
+# Downloads dependencies in an existing virtual environment and add the dependencies to the module path
+def bootstrap_model_downloader(venv_dir: str, dependencies: list[str]) -> None:
+    # download dependencies
+    subprocess.check_call([f"{venv_dir}/bin/pip", "install"] + dependencies, shell=False)
+    venv_modules: Path | None = next(Path(f"{venv_dir}/lib/").glob("python3.*/site-packages"), None)
+
+    if venv_modules is not None:
+        sys.path.insert(0, venv_modules.as_posix())
+    else:
+        raise RuntimeError
+
+
+def load_config_from_resources() -> dict[str,Any]:
+    config_text: str = importlib.resources.read_text("model_downloader", "config.toml")
+    return tomllib.loads(config_text)
+
+
+def orchestrate_model_download(args: list[str]) -> int:
+    config: dict[str,Any] = load_config_from_resources()
+
+    venv_manager: VenvManager = VenvManager("model_downloader")
+    venv_manager.create_venv()
+
+    try:
+        bootstrap_model_downloader(venv_manager.get_venv_dir(), config["model-repo"]["repo-provider"]["dependencies"])
+    except subprocess.CalledProcessError | RuntimeError as e:
+        print(
+            "Unable to download huggingface_hub dependency or " +
+            "unable to access the hugging_face module in virtual " +
+            f"environment: {venv_manager.get_venv_dir()}"
+        )
+        return 1
+
+    # import repo_interface
+    import repo_interface
+    # call and return repo_interface.main(args)
+    exit_code: int = repo_interface.download_model(config)
+    # delete temporary environment used for pulling AI model
+    venv_manager.remove_venv()
+
+    return exit_code
+
+if __name__ == "__main__":
+    sys.exit(orchestrate_model_download(sys.argv[1:]))
--- src/model_downloader/repo_interface.py
+++ src/model_downloader/repo_interface.py
@@ -15,12 +15,10 @@ class RepoConfig:
     revision: str
 
     @classmethod
-    def from_toml(cls: Type["RepoConfig"], toml_path: Path) -> "RepoConfig":
-        config: dict[str, Any] = tomllib.load(toml_path.open(mode="rb"))
-
+    def from_toml(cls: Type["RepoConfig"], config: dict[str,Any]) -> "RepoConfig":
         return cls(
             repo_id=config["model-repo"]["repo_id"],
-            repo_provider=config["model-repo"]["repo_provider"],
+            repo_provider=config["model-repo"]["repo-provider"]["provider"],
             repo_file=config["model-repo"]["repo_file"] if "repo_file" in config["model-repo"] else "",
             download_path=config["model-repo"]["download_path"] if "download_path" in config["model-repo"] else "",
             revision=config["model-repo"]["revision"] if "revision" in config["model-repo"] else ""
@@ -32,15 +30,16 @@ class RepoInterface(Protocol):
     def pull_file(self: "RepoInterface", repo_id: str, file_name: str, download_path: str = "", revision ="") -> None: ...
     def validate_configuration(cls: Type["RepoInterface"], config: RepoConfig) -> None: ...
     def _create_path(self: "repoInterface", path: str) -> Path | Exception:
-        if(Path.exists(path)):
-            return Path(path)
+        maybe_path: Path = Path(path)
+        if maybe_path.exists():
+            return maybe_path
         raise FileNotFoundError(f"'${path}' does not exist, there are insufficient" +
                                 "permissions to access the file or a parent directory, " +
                                 "or the file name contains invalid characters.")
 
-class Repo[T : RepoInterface]:
-    def __init__(self: "Repo", repo: T, repo_config: RepoConfig) -> None:
-        self.repo: T = repo
+
+class RepoFetchResolver[T : RepoInterface]:
+    def __init__(self: "RepoFetchResolver", repo: T, repo_config: RepoConfig) -> None:
         self.pull_repo_data: Union[Callable[[],None], Callable[[],None]]= \
             (lambda : repo.clone_repo(
                 repo_config.repo_id,
@@ -54,7 +53,6 @@ class Repo[T : RepoInterface]:
                 repo_config.download_path,
                 repo_config.revision
             ))
-
 
 
 class HuggingFaceRepoInterface(RepoInterface):
@@ -90,38 +88,27 @@ class RepoFactory:
     def __init__(self: "RepoFactory", repo_config: RepoConfig) -> None:
         self.repo_config: RepoConfig = repo_config
 
-    def build_repo_interface[T : RepoInterface](self: "RepoFactory") -> Repo[T]:
+    def build_repo_interface[T : RepoInterface](self: "RepoFactory") -> RepoFetchResolver[T]:
         """Create concrete repo interface if repo provider is known
 
-        :param repo_provider: str
         :raises UnknownRepoProviderException: if the repo provider is unknown
-        :return RepoInterface: if the repo type is valid:
+        :return RepoFetchResolver[T]: if the repo provider is valid:
         """
         match self.repo_config.repo_provider:
             case "huggingface":
                 repo_interface: RepoInterface = HuggingFaceRepoInterface()
                 repo_interface.validate_configuration(self.repo_config)
-                return Repo(repo_interface, self.repo_config)
+                return RepoFetchResolver(repo_interface, self.repo_config)
             case _:
                 raise RepoFactory.UnknownRepoProviderException(
                     f"Unknown repo provider: {self.repo_config.repo_provider}. Known repo providers are: 'huggingface'."
                 )
 
-def main(args: list[str]) -> int:
 
-    toml_path: Path = Path(args.pop(1)) \
-        if args != None and len(args) > 1 \
-        else Path("../../pyproject.toml")
-
-    if not toml_path.exists() and toml_path.is_file():
-        raise Exception("Please provide the path to the toml configuration file")
-
-    repo_config: RepoConfig = RepoConfig.from_toml(toml_path)
+def download_model(config: dict[str,Any]) -> int:
+    repo_config: RepoConfig = RepoConfig.from_toml(config)
     repo_factory: RepoFactory = RepoFactory(repo_config)
-    repo_interface: Repo[RepoInterface] = repo_factory.build_repo_interface()
+    repo_interface: RepoFetchResolver[RepoInterface] = repo_factory.build_repo_interface()
     repo_interface.pull_repo_data()
 
     return 0
-
-if __name__ == "__main__":
-    sys.exit(main(sys.argv))
