5fe8cd792638ed57dc700a41f428ead0061632b1
Ramirez, Russell <rrussell.public@gmail.com>
2026-07-22 20:51:51 -0500

    Testing new key.

--- src/cautious_meme/whisper-large-v3-turbo.py
+++ src/cautious_meme/whisper-large-v3-turbo.py
@@ -12,7 +12,7 @@ from pathlib import Path
 device = "cuda:0" if torch.cuda.is_available() else "cpu"
 torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
 
-model_id = "models"
+model_id = "models/openai/whisper-large-v3-turbo"
 
 model = AutoModelForSpeechSeq2Seq.from_pretrained(
     model_id, dtype=torch_dtype, low_cpu_mem_usage=True, use_safetensors=True
